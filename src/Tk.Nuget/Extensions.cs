364d43bcf27cf5f561319691a42aca205a6b046f
tonycknight <tonycknight@users.noreply.github.com>
2024-01-07 01:12:52 +0000

    cleanup

--- src/Tk.Nuget/Extensions.cs
+++ src/Tk.Nuget/Extensions.cs
@@ -23,17 +23,5 @@ namespace Tk.Nuget
             }
             return value;
         }
-
-        public static string NormaliseVersion(this string version)
-        {
-            version = version.Trim();
-            var shaIdx = version.IndexOf('+');
-            if (shaIdx >= 0)
-            {
-                return version.Substring(0, shaIdx);
-            }
-
-            return version;
-        }
     }
 }
