3838bc6600adf5584c2ee2892b2e5f4c5f5e642d
Tony Knight <tonycknight@users.noreply.github.com>
2026-03-13 19:22:50 +0000

    upgrade to xunit v3

--- tests/Tk.Nuget.Tests.Unit/StubPackageSearchMetadata.cs
+++ tests/Tk.Nuget.Tests.Unit/StubPackageSearchMetadata.cs
@@ -7,51 +7,51 @@ namespace Tk.Nuget.Tests.Unit
 {
     public record StubPackageSearchMetadata : IPackageSearchMetadata
     {
-        public string Authors { get; set; }
+        public string? Authors { get; set; }
 
         public IEnumerable<PackageDependencyGroup> DependencySets => throw new NotImplementedException();
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public long? DownloadCount { get; set; }
 
-        public Uri IconUrl { get; set; }
+        public Uri? IconUrl { get; set; }
 
-        public PackageIdentity Identity { get; set; }
+        public PackageIdentity? Identity { get; set; }
 
-        public Uri LicenseUrl { get; set; }
+        public Uri? LicenseUrl { get; set; }
 
-        public Uri ProjectUrl { get; set; }
+        public Uri? ProjectUrl { get; set; }
 
-        public Uri ReadmeUrl { get; set; }
+        public Uri? ReadmeUrl { get; set; }
 
-        public string ReadmeFileUrl { get; set; }
+        public string? ReadmeFileUrl { get; set; }
 
-        public Uri ReportAbuseUrl { get; set; }
+        public Uri? ReportAbuseUrl { get; set; }
 
-        public Uri PackageDetailsUrl { get; set; }
+        public Uri? PackageDetailsUrl { get; set; }
 
         public DateTimeOffset? Published { get; set; }
 
-        public IReadOnlyList<string> OwnersList { get; set; }
+        public IReadOnlyList<string>? OwnersList { get; set; }
 
-        public string Owners { get; set; }
+        public string? Owners { get; set; }
 
         public bool RequireLicenseAcceptance { get; set; }
 
-        public string Summary { get; set; }
+        public string? Summary { get; set; }
 
-        public string Tags { get; set; }
+        public string? Tags { get; set; }
 
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         public bool IsListed { get; set; }
 
         public bool PrefixReserved { get; set; }
 
-        public LicenseMetadata LicenseMetadata { get; set; }
+        public LicenseMetadata? LicenseMetadata { get; set; }
 
-        public IEnumerable<PackageVulnerabilityMetadata> Vulnerabilities { get; set; }
+        public IEnumerable<PackageVulnerabilityMetadata>? Vulnerabilities { get; set; }
 
         public Task<PackageDeprecationMetadata> GetDeprecationMetadataAsync()
         {
