703f802fbbc7cd40a92e5fa012f95db6c05bea8b
Tony Knight <tonycknight@users.noreply.github.com>
2026-06-20 14:14:35 +0100

    style

--- src/Tk.Nuget/NugetClient.cs
+++ src/Tk.Nuget/NugetClient.cs
@@ -108,6 +108,6 @@ namespace Tk.Nuget
             targetPath.ArgNotEmpty(nameof(targetPath));
 
             return await new NugetPackageDownloader().DownloadPackageAsync(packageId, version, targetPath, decompress, cancellation);
-        }        
+        }
     }
 }
